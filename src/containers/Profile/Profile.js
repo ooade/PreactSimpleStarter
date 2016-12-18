95287d8dc736439b6327924942ed3e338a504666
Adegbuyi Ademola <marhyorh@gmail.com>
2016-12-19 00:48:42 +0100

    Remove build folder

--- src/containers/Profile/Profile.js
+++ src/containers/Profile/Profile.js
@@ -5,7 +5,7 @@ export default class Profile extends Component {
 	render() {
 		return (
 			<div className='profile'>
-				<Helmet title="My Profile" />
+				<Helmet title="Developed by" />
 				<a href="http://github.com/ooade"> Ademola Adegbuyi </a>
 			</div>
 		);
